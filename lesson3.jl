dfbfb6bb8bc492c4a51172e128ac910c279df7a0
Colin Caine <cmcaine@gmail.com>
2020-05-16 13:26:47 +0100

    Update lesson3 with tiles

--- lesson3.jl
+++ lesson3.jl
@@ -3,8 +3,6 @@ Lesson 3 - Transparency initiative
 
 Show a smiley face with a transparent background over a tiled background of red and yellow text.
 
-The original tutorial writes a method for tiling the images, but I haven't bothered.
-
 """
 
 using SimpleDirectMediaLayer
@@ -28,6 +26,21 @@ function load_texture!(renderer, filename)
 end
 
 """
+    draw!(ren, tex::Ptr{SDL.Texture}, x, y, w, h)
+
+Draw texture to renderer at position x, y, scaled to width & height w, h.
+
+"""
+function draw!(ren, tex::Ptr{SDL.Texture}, x, y, w, h)
+    dst = SDL.Rect(Cint.((x, y, w, h))...)
+    # Don't know if I should be using Ref or pointer.
+    # dst is a regular Julia struct, so it's lifetime is controlled by Julia,
+    # so I'm going with Ref.
+    SDL.RenderCopy(ren, tex, C_NULL, Ref(dst))
+    return nothing
+end
+
+"""
     draw!(ren, tex::Ptr{SDL.Texture}, x, y)
 
 Draw texture to renderer at position x, y, preserving texture's width and height
@@ -35,11 +48,7 @@ Draw texture to renderer at position x, y, preserving texture's width and height
 """
 function draw!(ren, tex::Ptr{SDL.Texture}, x, y)
     w, h = texture_wh(tex)
-    dst = SDL.Rect(Cint.((x, y, w, h))...)
-    # Don't know if I should be using Ref or pointer.
-    # dst is a regular Julia struct, so it's lifetime is controlled by Julia,
-    # so I'm going with Ref.
-    SDL.RenderCopy(ren, tex, C_NULL, Ref(dst))
+    draw!(ren, tex, x, y, w, h)
     return nothing
 end
 
@@ -64,6 +73,7 @@ end
 function main()
     screen_width = 640
     screen_height = 480
+    tile_size = 40
 
     checkptr(SDL.Init(SDL.INIT_VIDEO))
 
@@ -72,21 +82,31 @@ function main()
         Cint.((100, 100, screen_width, screen_height))...,
         SDL.WINDOW_SHOWN))
 
-    # Flags don't seem important, so I've omitted them
-    renderer = checkptr(SDL.CreateRenderer(window, Cint(-1), Cuint(0)))
+    # -1 is for the "index of the rendering driver to initialise, or -1
+    # for the first one supporting the requested flags"
+    renderer = checkptr(SDL.CreateRenderer(window, Cint(-1), SDL.RENDERER_ACCELERATED))
 
     background = load_texture!(renderer, "res/Lesson3/background.png" )
     image = load_texture!(renderer, "res/Lesson3/image.png")
 
     for _ in 1:3
         SDL.RenderClear(renderer)
-        bW, bH = texture_wh(background)
-        draw!(renderer, background, 0, 0)
-        draw!(renderer, background, bW, 0)
-        draw!(renderer, background, 0, bH)
-        draw!(renderer, background, bW, bH)
+
+        # Tiled background
+        xtiles = screen_width ÷ tile_size
+        ytiles = screen_height ÷ tile_size
+        for i in 0:(xtiles * ytiles - 1)
+            x = (i % xtiles) * tile_size
+            y = (i ÷ xtiles) * tile_size
+            draw!(renderer, background, x, y, tile_size, tile_size)
+        end
+
+        # Foreground
         iW, iH = texture_wh(image)
-        draw!(renderer, image, screen_width ÷ 2 - iW ÷ 2, screen_height ÷ 2 - iH ÷ 2)
+        draw!(renderer, image,
+            screen_width ÷ 2 - iW ÷ 2,
+            screen_height ÷ 2 - iH ÷ 2)
+
         SDL.RenderPresent(renderer)
         SDL.Delay(Cuint(1000))
     end
